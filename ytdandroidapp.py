472e3764f3eca6ed70f36f4bfeb31a188e2c410e
PROgrammer <97875562+Walnusskeim@users.noreply.github.com>
2024-08-15 15:01:40 +0200

    Played arround with .kv formatting again

--- ytdandroidapp.py
+++ ytdandroidapp.py
@@ -1,58 +1,46 @@
 import time
 
 import kivy
-from pytube import YouTube
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from pytube import YouTube
 
 
 kivy.require('1.9.0')
 
+Builder.load_file('ytdownloader.kv')
 
-class GridLayout(GridLayout):
-    # Initialize ifinite keywords
-    def __init__(self, **kwargs):
-        # Call grid layout constructor
-        super(GridLayout, self).__init__(**kwargs)
 
-        # Set columns
-        self.cols = 1
+class MyGridLayout(Widget):
 
-        # Add widgets
-        self.add_widget(Label(text="YT-Downloader"))
+    url = ObjectProperty(None)
 
-        self.add_widget(Label(text="Gib hier deinen Link ein"))
 
-        self.url = TextInput(multiline=True)
-        self.add_widget(self.url)
-
-        self.download = Button(text="Download", font_size=32)
-        self.add_widget(self.download)
-
-        #self.download.bind(on_press=self.press)
-
-        self.add_widget(Label(text="(Das Video wird in den Download Ordner gespeichert)"))
-
-    def press(self, instance):
+    def press(self):
         url = self.url.text
-        try:
-            path = "/storage/emulated/0/Download/"
 
-            yt = YouTube(url)
-            stream = yt.streams.get_highest_resolution()
-            stream.download(path)
+        print("Das ist dein Link: ", url)
 
-        except:
-            self.add_widget(Label(text="Das war wohl kein YouTube Link!"))
-            self.url.text = ""
+        self.url.text = ""
+
+        #try:
+         #   path = "/storage/emulated/0/Download/"
+#
+ #           yt = YouTube(url)
+  #          stream = yt.streams.get_highest_resolution()
+   #         stream.download(path)
+#
+ #       except:
+  #          self.add_widget(Label(text="Das war wohl kein YouTube Link!"))
+   #         self.url.text = ""
 
 
-class YTDownloader(App):
+class YTD(App):
     def build(self):
-        return GridLayout()
+        return MyGridLayout()
+
 
 if __name__ == "__main__":
-    YTDownloader().run()
+    YTD().run()
